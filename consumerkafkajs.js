08d2c36090a39ce2d92ebcae5ef1346b231d47ad
Sai Manikanta Godavarthi <saig2304@gmail.com>
2021-03-23 20:08:42 -0500

    Create consumerkafkajs.js

---
+++ consumerkafkajs.js
@@ -0,0 +1,52 @@
+const { Kafka } = require("kafkajs");
+
+const kafka = new Kafka({
+  clientId: "NodeConfluentSasl",
+  brokers: ["BROKER_URL"],
+  ssl: true,
+  sasl: {
+    mechanism: "plain",
+    username: "USER_NAME/API_KEY",
+    password: "PASSWORD/API_KEY_SECRET",
+  },
+});
+
+const consumerDetails = {
+  groupId: "ConfluentManager",
+  topic: "Sample_Topic",
+};
+
+const consumerRun = async details => {
+  const consumer = kafka.consumer({
+    groupId: details.groupId,
+    fromBeginning: true,
+  });
+  await consumer.connect();
+
+  // // It's possible to start from the beginning of the topic
+  await consumer.subscribe({
+    topic: details.topic,
+    fromBeginning: true,
+  });
+
+  await consumer.run({
+    // autoCommit: false,
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log("message->", {
+        key: message.key ? message.key.toString() : "N/A",
+        value: message.value ? message.value.toString() : "N/A",
+        headers: message.headers,
+        topic,
+        partition,
+        offset: message.offset,
+        timestamp: message.timestamp,
+      });
+    },
+  });
+};
+
+consumerRun(consumerDetails).then(console.log).catch(console.log);
+
+module.exports = {
+  consumerRun,
+};
